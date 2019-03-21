cab9d5bc0f7011d7a7c21ffde29142a21e0b8b42
Daniel Speer <dan.speer11@gmail.com>
2019-03-21 14:49:15 -0700

    tst

--- _044.rb
+++ _044.rb
@@ -6,47 +6,37 @@ class PentagonalNumbers
 
   def initialize(size)
     @size = size
-    @pent_lst = [*1..size].map { |i| i * (3*i - 1) / 2 }
-    @pent = @pent_lst.to_set
 
+    @pent_set = [*1..size].to_set
+    @pent_prod_set = Set.new
+    @pent_diff_set = Set.new
+    
     @pass_lst = []
     @pass_min = 0
   end
 
   def main(target_len = 1)
-    check_lst
-    while @pass_lst.length < target_len
-      increase_size
-      check_lst
-    end
-    
-    @pass_min
+
   end
 
-  def check_lst
-    @pent_lst.each_with_index do |p1, i|
-      @pent_lst[(i + 1)..-1].each do |p2|        
-        if pentagonal_product?(p1, p2) && pentagonal_diff?(p1, p2)
-          @pass_lst.append(p2 - p1)
-          @pass_min = p2 - p1 if p2 - p1 < @pass_min
-        end
-      end
-    end
+  def pentagonal(i)
+    i * (3*i - 1) / 2
   end
 
-  def increase_size
-    @new_pent_lst = [*(@size + 1) .. @size * 2]
-    @size *= 2
-    @pent_lst += @new_pent_lst
-    @pent += @new_pent_lst.to_set
+  def pentagonal_prod(i1, i2)
+    pentagonal(i1) * pentagonal(i2)
   end
 
-  def pentagonal_product?(pent1, pent2)
-    @pent.include?(pent1 * pent2)
+  def petagonal_diff(i1, i2)
+    pentagonal(i2) - pentagonal(i1)
   end
 
-  def pentagonal_diff?(pent1, pent2)
-    @pent.include?(pent2 - pent1)
+  def is_pentagonal?(num)
+    a = 1.5
+    b = -0.5
+    c = -num
+
+    quadratic = -b 
   end
 end
 
